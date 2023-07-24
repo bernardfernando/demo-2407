f203046d07127aa60e7779f7c06dc0f04d730563
bernardfernando <jagathf@gmail.com>
2023-07-24 12:50:21 +0100

    new demo 24072023

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState({});
+
+  function handleSearch(event) {
+    setSearchQuery(event.target.value);
+  }
+
+  async function getLocation() {
+    const API= 
+
+    const res = await axios.get(API)
+
+    setLocation(res.data[0]);
+
+    handleMap(re.data[0])   // invole soon after the above
+  }
+function handleMap(data) {
+  console.log(data);
+const API = 
+const res = axios.get(API)
+}
+
+// replace above with this
+async function getLocation(event){
+  try {event.preventDefault();
+  event.target.input.value ="";
+  
+} catch 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getLocation}></form>
+      <input
+        type="text"
+        placeholder="search for a city"
+        onChange={handleSearch}
+      />
+      <button onClick={getLocation}>Explore</button>
+      <p>{location.display_name}</p>
+      {map && <img src={map} alt=""}
     </div>
   );
 }
